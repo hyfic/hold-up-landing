a2d6f79854dbc0de3a3d9263c14c89cf02540f55
Fadhil <fadhilsaheer@gmail.com>
2023-05-30 06:24:02 +0530

    feat: content

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,6 @@
-<div>
-  <h1 class="text-3xl font-medium">Hold Up</h1>
+<div class="h-screen w-full flex flex-col items-center justify-center">
+  <img src="/logo_text.svg" class="w-40" alt="hold up" />
+  <h2 class="text-7xl text-center mt-5 font-medium tracking-wide leading-tight">
+    take control of<br /> your life for good.
+  </h2>
 </div>
